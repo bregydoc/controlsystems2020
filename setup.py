ad2a1484ad68116c89411a00cff849f3726c7640
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-06-10 09:24:49 -0500

    new deps for pipenv

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="control2020",
-    version="0.0.1",
+    version="0.0.2",
     author="Bregy Malpartida",
     author_email="bregy.malpartida@utec.edu.pe",
     description="A toolkit to design and lear about control systems",
