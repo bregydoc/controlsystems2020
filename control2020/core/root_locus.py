a70e22c6815e0d0bdcd5d201e0a49a84c8b00c15
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-06-11 03:44:59 -0500

    rewrite the plot root locus function

--- control2020/core/root_locus.py
+++ control2020/core/root_locus.py
@@ -1,18 +1,17 @@
 import numpy as np
 import sympy as sp
+import control as ct
 from matplotlib import pyplot as plt
 from typing import Tuple, Union, List
 from control2020 import core
 
 
-def plot_root_locus(g: sp.Expr, k: sp.Expr = sp.var("K"), h: sp.Expr = 1,
-                    ki: float = 0, kf: float = 5e3, points: int = 500, k_space: str = "lin",
-                    print_critical: bool = False, critical_tolerance: float = 0.1) -> plt.Line2D:
+def plot_root_locus(g: Union[sp.Expr, ct.TransferFunction], ki: float = 0, kf: float = 5e3,
+                    points: int = 500, k_space: str = "lin", print_critical: bool = False,
+                    critical_tolerance: float = 0.01) -> plt.Line2D:
     """
     Plot a medium fidelity Root Locus of your G, K and H transfer functions
     :param g: Your plant transfer function
-    :param k: Your gain (or compensator) transfer function
-    :param h: Your feedback transfer function
     :param ki: Gain range start (default: 0)
     :param kf: Gain range end (default: 5000)
     :param points: Fine Grain or measured points of your Gain range (default: 500)
@@ -21,16 +20,17 @@ def plot_root_locus(g: sp.Expr, k: sp.Expr = sp.var("K"), h: sp.Expr = 1,
     :param critical_tolerance: The minimal threshold distance to imag axis
     :return: A Plot Line2D object, ready to show
     """
-    plt.title(f"Root locus of $G={sp.latex(g)}$", fontsize='x-large')
+    # s = sp.var("s")
+
+    if isinstance(g, ct.TransferFunction):
+        sys = g
+    else:
+        sys = core.symbolic_transfer_function(g)
+
+    # plt.title(f"Root locus of $G={sp.latex(g)}$", fontsize='x-large')
     plt.xlabel("$Real\\ Axis\\ [s^{-1}]$")
     plt.ylabel("$Imaginary\\ Axis\\ [s^{-1}]$")
 
-    eq = core.extract_characteristic_equation(k, g, h)
-    eq = eq.n(chop=True)
-
-    s = sp.var("s")
-
-    poles_points = {}
     if k_space == "lin":
         range_space = np.linspace(ki, kf, points)
     elif k_space == "log":
@@ -38,30 +38,26 @@ def plot_root_locus(g: sp.Expr, k: sp.Expr = sp.var("K"), h: sp.Expr = 1,
     else:
         raise BaseException("invalid space, use \"lin\" o \"log\"")
 
-    for current_gain in range_space:
-        p = sp.Poly(sp.expand(eq.subs(sp.var("K"), current_gain)), s)
-        all_coeffs = list(p.all_coeffs())
-        num_polynomial = np.poly1d(all_coeffs)
-        points = list(np.roots(num_polynomial))
-        for i, point in enumerate(points):
+    paths, gains = ct.root_locus(sys, kvect=range_space, Plot=False)
+
+    for i, gain in enumerate(gains):
+        points = paths[i]
+        for point in points:
             if print_critical and abs(point.real) < critical_tolerance:
-                print("Critical gain K = %.3f at %.3f + %.3fi" % (current_gain, point.real, point.imag))
-            if i not in poles_points:
-                poles_points[i] = []
-            poles_points[i].append(point)
+                print("Critical gain K = %.3f at %.3f + %.3fi" % (gain, point.real, point.imag))
 
     plt.axvline(0, color='k')
     plt.axhline(0, color='k')
     plt.grid()
 
-    for pole in poles_points.keys():
-        plt.plot(np.real(poles_points[pole]), np.imag(poles_points[pole]))
+    plt.plot(np.real(paths), np.imag(paths))
 
-    p = sp.Poly(sp.expand(eq.subs(sp.var("K"), 0)), s)
-    num_polynomial = np.poly1d(p.all_coeffs())
-    k0_poles = list(np.roots(num_polynomial))
+    n_poles = sys.pole()
+    n_zeros = sys.zero()
 
-    return plt.plot(np.real(k0_poles), np.imag(k0_poles), "kX")
+    plt.plot(np.real(n_poles), np.imag(n_poles), "kx")
+
+    return plt.plot(np.real(n_zeros), np.imag(n_zeros), "ko")
 
 
 def find_points_in_root_locus(g: sp.Expr, find: Union[List[float], List[complex]], k: sp.Expr = sp.var("K"),
